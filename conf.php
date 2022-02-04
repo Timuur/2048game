0c14014722d14c1f24a39af305570949795ca0d9
Timuur <timur2st@gmail.com>
2022-02-04 14:49:34 +0500

    sdf

--- conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conn = new mysqli('localhost', 'root', 'root', '1m');
-$conn->set_charset('UTF8');
-
-CONST PATH = '/view/';
-?>
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    include './conf.php';
-    include './controller/controller.php';
-
-?>
